73eba2c84d46826fed1548192c52b0bf32c606c6
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2019-11-21 18:08:26 +0100

    #2: Add eslint verification for TS files. Close #2

--- src/Controller/Home.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { Request, Response } from "express";
-
-export const home = (req: Request, res: Response) => {
-  res.render("home", {
-    title: "Home"
-  });
-};
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-'use strict';
+import express from 'express';
+import path from 'path';
+import morgan from 'morgan';
 
-import express from "express"
-import path from "path"
-import morgan from "morgan"
-
-import * as homeController from "./Controller/Home"
+import HomeController from './Controller/HomeController';
 
 const app: express.Application = express();
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "../views"));
-app.use(morgan("combined"));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '../views'));
+app.use(morgan('combined'));
 
-app.get("/", homeController.home);
+const homeController = new HomeController();
+app.get('/', homeController.get);
 
 module.exports = app;
