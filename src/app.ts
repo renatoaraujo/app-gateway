04f6ce4d7d336f962f66f31077efc3ba13c65cb0
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2019-11-22 14:12:39 +0100

    #3: Add logger to application with some configuration on app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import path from 'path';
 import morgan from 'morgan';
-
+import Logger from "./Infrastructure/Decorator/Logger";
 import HomeController from './Controller/HomeController';
+import RootPath from "app-root-path";
+import * as fs from "fs";
 
 const app: express.Application = express();
+
+// Initialize needed folders
+const varDir = path.join(RootPath.toString(), 'var');
+if (!fs.existsSync(varDir)){
+  fs.mkdirSync(varDir);
+}
+
+// Initialize logger
+const logger: Logger = new Logger();
+logger.init();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '../views'));
